698b2953170d4f956a394271a081ae6fc37360f5
JEFFe <andrejeff.leevi@gmail.com>
2021-01-02 21:51:41 +0200

    Korjattu tuo ei toimi popup roska

--- background.js
+++ background.js
@@ -1,10 +1,10 @@
-// chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
-//   if (changeInfo.status == 'complete') {
-//     if (tab.url.match('^https?://(www\.)?instagram.com*')) {
-//       chrome.tabs.executeScript(tab.id, { file: 'script.js' });
-//     }
-//   }
-// });
+chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
+  if (changeInfo.status == 'complete') {
+    if (tab.url.match('^https?://(www\.)?instagram.com*')) {
+      chrome.tabs.executeScript(tab.id, { file: 'script.js' });
+    }
+  }
+});
 
 chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
   if ((request.msg).search('DL') != -1) {
